d1ec1e3493bbba0b5ab758ff7f6108ff037d2963
Nicholas <nicholaskerriss7@gmail.com>
2023-06-14 13:01:09 -0300

    almost ready

--- js/script.js
+++ js/script.js
@@ -5,19 +5,66 @@
 
 // Adiciona o evento de clique ao botão "Calcular"
 document.getElementById("calcular").addEventListener("click", calcularIMC);
-
-// Função para calcular o IMC
 function calcularIMC() {
   // Obter os valores de peso e altura
-  var peso = parseFloat(document.getElementById("peso").value);
-  var altura = parseFloat(document.getElementById("altura").value);
+  let peso = parseFloat(document.getElementById("peso").value);
+  let altura = parseFloat(document.getElementById("altura").value);
 
   // Calcular o IMC
-  var imc = peso / (altura / 100) ** 2;
+  let imc = peso / (altura / 100) ** 2;
 
   // Obter o elemento de resultado
-  var resultadoElemento = document.getElementById("resultado");
+  let resultadoElemento = document.getElementById("resultado");
 
-  // Atualizar o valor do elemento de resultado
-  resultadoElemento.value = "Seu IMC é: " + imc.toFixed(2);
+  // Atualizar o conteúdo do elemento de resultado
+  resultadoElemento.textContent = "Seu IMC é: " + imc.toFixed(2);
+}
+
+// Função para calcular o IMC
+// function calcularIMC() {
+//   // Obter os valores de peso e altura
+//   let peso = parseFloat(document.getElementById("peso").value);
+//   let altura = parseFloat(document.getElementById("altura").value);
+
+//   // Calcular o IMC
+//   let imc = peso / (altura / 100) ** 2;
+
+//   // Obter o elemento de resultado
+//   let resultadoElemento = document.getElementById("resultado");
+
+//   // Atualizar o valor do elemento de resultado
+//   resultadoElemento.value = "Seu IMC é: " + imc.toFixed(2);
+// }
+
+function classificarIMC() {
+  // Obter o valor do IMC calculado
+  let imc = parseFloat(document.getElementById("resultado").value);
+
+  // Variável para armazenar a classificação
+  let classificacao;
+
+  switch (true) {
+    case imc < 18.5:
+      classificacao = "Baixo Peso";
+      break;
+    case imc < 24.9:
+      classificacao = "Peso Normal";
+      break;
+    case imc < 29.9:
+      classificacao = "Sobrepeso";
+      break;
+    case imc < 34.9:
+      classificacao = "Obesidade Grau I";
+      break;
+    case imc < 39.9:
+      classificacao = "Obesidade Grau II";
+      break;
+    default:
+      classificacao = "Obesidade Grau III";
+      break;
+  }
+
+  // Atualizar o valor do elemento de classificação
+  let classificacaoElemento = document.getElementById("classificacao");
+  classificacaoElemento.textContent = "Classificação: " + classificacao;
 }
